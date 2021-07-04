a599587c36a62e232069d83b25a7837c5e405a8f
ASYRAF\Asyraf Jamil <asyrafjamil7@gmail.com>
2021-07-04 18:50:09 +0800

    Chore: wrote jsDoc for all helpers file

--- src/helpers/filterProduct.js
+++ src/helpers/filterProduct.js
@@ -1,4 +1,13 @@
 const listOfProducts = require('../../products.json');
+
+/**
+ * Helper method
+ *
+ * @description Find product based on productId
+ * @param {*} productId
+ *
+ */
+
 exports.filterProduct= function(productId) {
   return listOfProducts.filter((item) =>
     item.upc === productId);
--- src/helpers/sanitizeMessage.js
+++ src/helpers/sanitizeMessage.js
@@ -1,3 +1,11 @@
+/**
+ * Helper method
+ *
+ * @description Remove white spaces and other unnecessary strings
+ * from the message and split the messages to an array
+ * @param {*} messageString
+ */
+
 exports.sanitizeMessage= function(messageString) {
   return messageString.toLowerCase().trim().split(/[ ,]+/);
 };
